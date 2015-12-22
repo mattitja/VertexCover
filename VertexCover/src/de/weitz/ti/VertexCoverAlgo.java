53cd4e03ff2e7e6e490221e19146d40254f5a85b
pete7 <matthias.miro@gmail.com>
2015-12-22 17:30:22 +0100

    added gitignore file

--- VertexCover/src/de/weitz/ti/VertexCoverAlgo.java
+++ VertexCover/src/de/weitz/ti/VertexCoverAlgo.java
@@ -3,12 +3,7 @@
 package de.weitz.ti;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 class VertexCoverAlgo {
